15c091da4b75d1a72df9a6858184b97e236d5f4f
rakibi-alaa <rakibi.alae@gmail.com>
2020-06-14 13:47:21 +0100

    project init

--- App.js
+++ App.js
@@ -1,19 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createSharedElementStackNavigator } from 'react-navigation-shared-element';
+import Home from './src/screens/home';
+const Stack = createSharedElementStackNavigator();
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="List">
+        <Stack.Screen name="Home" component={Home} />
+        {/* <Stack.Screen
+          name="Detail"
+          component={DetailScreen}
+          sharedElements={(route, otherRoute, showing) => {
+            const { item } = route.params;
+            return [`item.${item.id}.photo`];
+          }}
+        /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
+export default App;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ src/screens/home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {SafeAreaView,View,Text} from 'react-native'
+
+
+export default class Home extends React.Component{
+    render(){
+        return(
+            <SafeAreaView>
+                <View>
+                    <Text>test welcome</Text>
+                </View>
+            </SafeAreaView>
+        )
+    }
+}
