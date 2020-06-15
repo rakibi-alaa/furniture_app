e7240c4368269216bb48e1f97e1907efc66784c3
rakibi-alaa <rakibi.alae@gmail.com>
2020-06-15 13:20:42 +0100

    Articles list Done

--- src/components/list.js
+++ src/components/list.js
@@ -17,29 +17,64 @@ const BackDrop = styled.TouchableOpacity`
     position : absolute;
     bottom : 0px;
     left : 0px;
-    z-index : -1;
+    z-index : -10;
 `
 const Item = styled.View`
     width : 100%;
     height : 150px;
     border-radius : 20px;
     background-color : #FFF;
-    margin-bottom : 25px;;
+    margin-bottom : 45px;
+    z-index : -1;
+    position : relative;
+`
+const ItemInnerContainer = styled.View`
+    flex-direction : row;
+    width : 100%;
+    height:100%;
+    border-radius :20px;
+    
+    
+`
+const ItemDetail = styled.View`
+   width:50%;
+   overflow : hidden;
+   height : 100%;
+   padding-left : 20px;
+   padding-top :20px;
+   position:relative;
+`
+const ItemTitle = styled.Text`
+    font-size : 20px;
+    font-weight : 600;
+`
+const ItemPriceContainer = styled.View`
+    background-color:pink;
+    position:absolute;
+    bottom :0px;
+    padding : 8px 15px;
 `
 
-const ItemDetail = styled.Text`
-   
-`
+
 const _renderItem = ({item,index}) =>{
+    console.log(item.image)
     return (
-        <TouchableWithoutFeedback>
-            <Item key={item.id} style={{borderRightWidth : 5,borderRightColor: '#000',shadowColor: "#000",shadowOffset: {width: 0,height: 2,},shadowOpacity: 0.13,shadowRadius: 2.62,elevation: 2}}>
-                <ItemDetail>
+        <TouchableWithoutFeedback onPress={()=>{alert('here')}}>
+            <Item key={item.id} style={{borderRightWidth : 5,borderRightColor: index %2 == 0 ? '#329ba8' : 'coral',shadowColor: "#000",shadowOffset: {width: 0,height: 2,},shadowOpacity: 0.13,shadowRadius: 2.62,elevation: 2}}>
+                <ItemInnerContainer >
+                    <ItemDetail style={{borderRadius : 20}}>
+                        <ItemTitle>{item.title}</ItemTitle>
+                        <ItemPriceContainer style={{borderTopRightRadius : 20}}>
+                            <Text>{item.price} $</Text>
+                        </ItemPriceContainer>
+                    </ItemDetail>
                     
-                </ItemDetail>
-                <SharedElement id={`item.${item.id}.photo`}>
-                    <Image source={item.photoUrl} />
-                </SharedElement>
+                    <SharedElement width="50%"  id={`item.${item.id}.photo`}>
+                        <Image source={{uri :item.image}} style={{width : 170,height : 160,top:-30,alignSelf: 'center'}} />
+                    </SharedElement>
+                   
+                    
+                </ItemInnerContainer>
             </Item>
         </TouchableWithoutFeedback>
     )
@@ -51,8 +86,8 @@ export default function List() {
             <FlatList
             style={{marginBottom : 150}}
                 showsVerticalScrollIndicator={true}
-                contentContainerStyle={{paddingHorizontal:20,}}
-                data={data.categories} 
+                contentContainerStyle={{paddingHorizontal:20,paddingTop:30}}
+                data={data.products} 
                 renderItem={_renderItem}
                 keyExtractor={item => item.id + ''}/>
         </Container>
--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -4,15 +4,15 @@ import styled from 'styled-components';
 import Svg, { Circle, Line,Path,G } from 'react-native-svg';
 
 const Container = styled.View`
-width : 100%;
-height : 50px;
+width : 90%;
+height : 40px;
 flex-direction : row;
 justify-content : space-between;
 align-items : center;
 padding : 0px 10px;
 border-radius : 6px;
 background-color : rgba(255,255,255,.4);
-margin : 15px 0px;
+margin : 15px auto;
 `
 const Input = styled.TextInput`
     width : 88%;
@@ -24,7 +24,7 @@ const Input = styled.TextInput`
 export default function SearchBar() {
     return (
             <Container >
-                <Svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="32" height="32" viewBox="0 0 24 24" stroke-width="3" stroke="#FFF" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <Svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="28" height="28" viewBox="0 0 24 24" stroke-width="3" stroke="#FFF" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <Path stroke="none" d="M0 0h24v24H0z" />
                     <Circle cx="10" cy="10" r="7" strokeWidth="2"/>
                     <Line x1="21" y1="21" x2="15" y2="15" strokeWidth="2" />
